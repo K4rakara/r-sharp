af84215520143ea802d28f01072060ea11497ad4
Jack Johannesen <jack@insertdomain.name>
2020-05-22 12:38:25 -0400

    Update utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,27 @@
+declare global
+{
+	interface Date
+	{
+		addSeconds(s: number): Date;
+		inSeconds(): number;
+		isPast(d: Date): boolean;
+	}
+}
 
-//@ts-ignore
-Date.prototype.addSeconds = function(s: number)
+Date.prototype.addSeconds = function(s: number): Date
 {
 	this.setSeconds(this.getSeconds() + s);
 	return this;
+}
+
+Date.prototype.inSeconds = function(): number
+{
+	return Math.round(this.getTime() / 1000);
+}
+
+Date.prototype.isPast = function(d: Date): boolean
+{
+	return (this.inSeconds() > d.inSeconds());
 }
 
 export const _ = null;
