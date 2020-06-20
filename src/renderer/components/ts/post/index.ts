e96eb42f2e72cee5f0aa0b0b4d4d1c298e3cddfd
K4rakara <jack@insertdomain.name>
2020-06-19 20:19:05 -0400

    Tidy some files

--- src/renderer/components/ts/post/index.ts
+++ src/renderer/components/ts/post/index.ts
@@ -1,6 +1,5 @@
 export { Post } from './post';
-export { PostVoting } from './post-voting';
 export { PostButtons } from './post-buttons';
-export { PostHeader } from './post-header';
-export { PostPreview } from './post-preview';
 export { PostConstructor, PostChildConstructor, RedditLink } from './interfaces';
+export { PostPreview } from './post-preview';
+export { PostVoting } from './post-voting';
